496c5ce95d0e3ad43b3fe5cd575034ac9c3dc154
Grace <gracelo0717@gmail.com>
2024-02-17 17:54:09 -0500

    passed all test

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  return cats.shift(name);
+}
+
+function appendCat(name) {
+  let arr = cats.slice();
+  arr.push(name);
+  return arr;
+}
+
+function prependCat(name) {
+  let arr = cats.slice();
+  arr.unshift(name);
+  return arr;
+}
+
+function removeLastCat(name) {
+  let arr = cats.slice();
+  arr.pop(name);
+  return arr;
+}
+
+function removeFirstCat(name) {
+  let arr = cats.slice();
+  arr.shift(name);
+  return arr;
+}
